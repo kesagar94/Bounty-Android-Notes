5d9cd84b129b141cbb4eb03ac7f084a87994eb36
kesagar94 <kesagar94@gmail.com>
2015-03-18 11:34:19 +0530

    started off

---
+++ src/com/keshav/bounty/First.java
@@ -0,0 +1,17 @@
+package com.keshav.bounty;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class First extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	
+
+}
